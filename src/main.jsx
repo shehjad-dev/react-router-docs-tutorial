006b8730fdccff530a6a1e5d450c4ba160950653
shehjad-dev <satauskz@gmail.com>
2022-02-16 20:55:23 +0600

    "Learnt from start to index routes"

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,39 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import Invoices from './routes/invoices';
+import Invoice from './routes/invoice';
+import Expenses from './routes/expenses';
 import './main.css';
 
 let rootElement = document.getElementById('root');
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="invoices" element={<Invoices />}>
+          <Route
+            index
+            element={
+              <main style={{ padding: '1rem' }}>
+                <p>Select an invoice</p>
+              </main>
+            }
+          />
+          <Route path=":invoiceId" element={<Invoice />} />
+        </Route>
+        <Route path="expenses" element={<Expenses />} />
+      </Route>
+      <Route
+        path="*"
+        element={
+          <main style={{ padding: '1rem' }}>
+            <p>There's nothing here!</p>
+          </main>
+        }
+      />
+    </Routes>
   </BrowserRouter>,
   rootElement
 );
